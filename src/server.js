ce6b7642277054d94c55f40e2b398b5e6a978089
Tawan Silva <tawan.sf.99@gmail.com>
2019-05-21 19:34:36 -0300

    configuracao do socket.io

--- src/server.js
+++ src/server.js
@@ -8,13 +8,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-
+const cors = require('cors');
 const conn = require('../config.json');
 
 const app = express();
+app.use(cors());
+
+// Socket.io
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    });
+});
+
+// Mongo
 
 mongoose.connect(conn.db, {
     useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
 });
 
 app.use(express.json());
@@ -23,4 +42,4 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require('./routes'));
 
-app.listen(3000);
+server.listen(3000);
